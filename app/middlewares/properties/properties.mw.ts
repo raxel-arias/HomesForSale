79e549c8ec5d196a1442fa2a152fd0646bd13267
AlbaSly <arias.brando.raxel@gmail.com>
2022-08-02 20:38:14 -0500

    Add EditProperty middleware at properties.mw.ts

--- app/middlewares/properties/properties.mw.ts
+++ app/middlewares/properties/properties.mw.ts
@@ -38,6 +38,66 @@ export const CreateProperty = async (req: Request, res: Response): Promise<void>
     }
 }
 
+export const EditProperty = async (req: Request, res: Response): Promise<void> => {
+    const user = <User> req.user;
+    const property = <Property> res.locals.property;
+    
+    try {
+        let inputErrors: Record<string, ValidationError>;
+        const result: Result<ValidationError> = validationResult(req);
+        
+        if (!result.isEmpty()) {
+            inputErrors = result.mapped();
+            
+            req.flash('input-errors', inputErrors);
+            req.flash('input-data', req.body);
+
+            res.redirect(`/me/property/edit/${property.id_property}`);
+
+            return;
+        }
+
+        let {
+            _csrf,
+
+            full_address, 
+            latitude, 
+            longitude, 
+
+            ...propertyToUpdateData
+        } = req.body;
+
+        let propertyToUpdate: Property = Object.assign(
+            {}, 
+            propertyToUpdateData, 
+            {
+                vendor: user.id_user, 
+                id_property: property.id_property
+            }); 
+        let locationToUpdate: Location = {
+            address: full_address, 
+            latitude, 
+            longitude
+        };
+
+        const propertyModification: PropertyModification = {
+            ...propertyToUpdate,
+            location: locationToUpdate
+        }
+        
+        const response = await new PropertyController().UpdateProperty(propertyModification);
+        
+        req.flash('req-info', response);
+
+        res.redirect('/me');
+    } catch (error: any) {
+        req.flash('req-error', error);
+        req.flash('input-data', req.body);
+        
+        res.redirect(`/me/property/edit/${property.id_property}`);
+    }
+}
+
 export const ValidatePropertyExistence = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
     const user = <User> req.user;
     const {id_property} = req.params;
