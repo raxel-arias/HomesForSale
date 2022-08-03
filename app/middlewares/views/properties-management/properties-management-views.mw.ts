b540454d6a7c6f4d38a1f69746dc90da9faa7d59
AlbaSly <arias.brando.raxel@gmail.com>
2022-08-02 20:41:42 -0500

    Add ShowEditPropertyView middleware view at properties-management-views.mw.ts

--- app/middlewares/views/properties-management/properties-management-views.mw.ts
+++ app/middlewares/views/properties-management/properties-management-views.mw.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express";
-import { CategoriesController } from "../../../controllers/categories/categories.controller";
 import { PropertyController } from "../../../controllers/properties/property.controller";
+import { CategoriesController } from "../../../controllers/categories/categories.controller";
+import { LocationController } from "../../../controllers/locations/locations.controller";
 import { Property, User } from '../../../interfaces/models/models.interface';
 import { ResolveResponse } from "../../../interfaces/response.interface";
 
@@ -48,6 +49,46 @@ export const ShowCreatePropertyView = async (req: Request, res: Response): Promi
             subtitle: 'Creating Property',
             categoriesList,
             user,
+    
+            errors,
+            info: requestInfo,
+            inputs,
+            csrfToken: req.csrfToken(),
+        });
+        
+    } catch (error: any) {
+        req.flash('req-error', error);
+
+        res.redirect('/app/index');
+    }
+}
+
+//Form
+export const ShowEditPropertyView = async (req: Request, res: Response): Promise<void> => {
+    let inputErrors: any = req.flash('input-errors')[0] || {};
+    let requestError: any = req.flash('req-error')[0] || {};
+    let requestInfo: any = req.flash('req-info')[0] || {};
+
+    const errors = {inputErrors, requestError};
+    const inputs = req.flash('input-data')[0];
+  
+    const property: Property = <Property> res.locals.property;
+    const user: User = <User>req.user;
+    
+    try {
+        
+        const [{data: {locationFound}}, {data: {categoriesList}}] = await Promise.all([
+            <ResolveResponse> await new LocationController().GetLocation(property.id_property!),
+            <ResolveResponse> await new CategoriesController().CategoriesList()
+        ]);
+        
+        res.render('management/edit-property', {
+            title: 'Edit Property',
+            subtitle: property.title,
+            user,
+            property,
+            locationFound,
+            categoriesList,
     
             errors,
             info: requestInfo,
