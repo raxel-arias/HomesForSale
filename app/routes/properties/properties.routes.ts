25d3d9acd7a94061b535620cf35f7c73bfb7566d
AlbaSly <arias.brando.raxel@gmail.com>
2022-08-17 01:11:48 -0500

    Add HTTP methods (POST, PATCH, DELETE) to PropertiesRouter and PropertiesRouter

--- app/routes/properties/properties.routes.ts
+++ app/routes/properties/properties.routes.ts
@@ -2,16 +2,27 @@ import { Router } from "express";
 import { 
     SetImage,
     CreateProperty, 
-    ValidatePropertyExistence
+    ValidatePropertyExistence,
+    EditProperty,
+    ChangePropertyStatus,
+    DeleteProperty,
+    GetPublicProperties
 } from "../../middlewares/properties/properties.mw";
 import { JWT } from "../../handlers/Auth.handler";
 import { PropertyValidator } from "../../validations/property.validator";
 import { uploader } from "../../middlewares/files.mw";
     
 export const PropertiesRouter: Router = Router();
+//Public access
+PropertiesRouter.get('/properties/public/list', GetPublicProperties);
 
 const PropertiesRouterChilds: Router = Router({mergeParams: true});
+
 PropertiesRouter.use('/properties', JWT.ValidateJWT, PropertiesRouterChilds);
 
+//Private Access
 PropertiesRouterChilds.post('/create', PropertyValidator.CreateProperty, CreateProperty);
 PropertiesRouterChilds.post('/set-image/:id_property', ValidatePropertyExistence, uploader.single('image'), SetImage);
+PropertiesRouterChilds.post('/edit/:id_property', ValidatePropertyExistence, PropertyValidator.CreateProperty, EditProperty);
+PropertiesRouterChilds.patch('/change-status/:id_property', ValidatePropertyExistence, ChangePropertyStatus);
+PropertiesRouterChilds.delete('/delete/:id_property', ValidatePropertyExistence, DeleteProperty);
