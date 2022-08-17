55ac506ff8c6969f807c986c7458e867846ada11
AlbaSly <arias.brando.raxel@gmail.com>
2022-08-17 00:59:03 -0500

    Add views for routes requests in PropertiesRouter and PropertiesRouterChilds

--- app/routes/views/properties-views.routes.ts
+++ app/routes/views/properties-views.routes.ts
@@ -1,12 +1,16 @@
 import { Router } from "express";
 import { 
-    ShowIndexView } from "../../middlewares/views/properties/properties-views.mw";
+    ShowIndexByGlobalMapView,
+    ShowIndexView, ShowPublicPropertyView } from "../../middlewares/views/properties/properties-views.mw";
+import { SetViewAsPublic } from "../../middlewares/views/auth/auth-views.mw";
 import { JWT } from '../../handlers/Auth.handler';
 
 export const PropertiesViewRouter: Router = Router();
 
 const PropertiesViewRouterChilds: Router = Router({mergeParams: true});
 
-PropertiesViewRouter.use('/app', JWT.ValidateJWT, PropertiesViewRouterChilds);
+PropertiesViewRouter.use('/app', PropertiesViewRouterChilds);
 
-PropertiesViewRouterChilds.get('/index', ShowIndexView);
+PropertiesViewRouterChilds.get('/index', SetViewAsPublic, JWT.ValidateJWT, ShowIndexView);
+PropertiesViewRouterChilds.get('/index/global', SetViewAsPublic, JWT.ValidateJWT, ShowIndexByGlobalMapView);
+PropertiesViewRouterChilds.get('/property/view/:id_property', SetViewAsPublic, JWT.ValidateJWT, ShowPublicPropertyView);
