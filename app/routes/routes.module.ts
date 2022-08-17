c7361d616c106e4918df9499bbc9c5e96f46a3ee
AlbaSly <arias.brando.raxel@gmail.com>
2022-08-17 01:22:07 -0500

    Add PropertiesManagementViewsRouter and PropertiesRouter in RouteDeclarations array at routes.module.ts

--- app/routes/routes.module.ts
+++ app/routes/routes.module.ts
@@ -1,8 +1,11 @@
 import { RouteDeclarator } from "../interfaces/routes.interface";
 
 import { AuthRouter } from "./auth/auth.routes";
+import { MessagesRouter } from "./messages/messages.routes";
+import { MiscellaneousRouter } from "./miscellaneous/miscellaneous.routes";
 import { PropertiesRouter } from "./properties/properties.routes";
 import { AuthViewsRouter } from "./views/auth-views.routes";
+import { MessagesViewsRouter } from "./views/message-views.routes";
 import { PropertiesManagementViewsRouter } from "./views/properties-management-views.routes";
 import { PropertiesViewRouter } from "./views/properties-views.routes";
 
@@ -13,6 +16,9 @@ export const RouteDeclarations: RouteDeclarator = {
         AuthRouter,
         PropertiesViewRouter,
         PropertiesManagementViewsRouter,
-        PropertiesRouter
+        PropertiesRouter,
+        MessagesRouter,
+        MessagesViewsRouter,
+        MiscellaneousRouter
     ]
 }
